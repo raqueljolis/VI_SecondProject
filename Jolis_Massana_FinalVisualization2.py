f095ac089342d431ec8bc78eea0f29b0021a3b7b
martinamassanam <150825304+martinamassanam@users.noreply.github.com>
2024-11-22 11:04:04 +0100

    Add files via upload

---
+++ Jolis_Massana_FinalVisualization2.py
@@ -0,0 +1,10 @@
+#################### Q1 ####################
+# - linechart, 5 lines, a line per region, being able to choose years
+# - map with regions, select state --> linechart with all states of the region
+
+#################### Q2 ####################
+# - grouped barcharts, pair of bars, one for 2014 and the other for the chosen year
+# - scatterplot with 12 points (1 per month), 2014 in x-axis, chosen year in y-axis
+
+#################### Q3 ####################
+# - map of states, select state and show a map for counties, slider to see years 
