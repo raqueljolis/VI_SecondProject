88df49a5165c66d0afcec01277d4e1b07e260487
martinamassanam <150825304+martinamassanam@users.noreply.github.com>
2024-11-29 10:09:55 +0100

    afegir comentaris Q1 i Q3

--- Jolis_Massana_FinalVisualization.py
+++ Jolis_Massana_FinalVisualization.py
@@ -1,4 +1,36 @@
-#################### Q2 ####################
+import streamlit as st
+import pandas as pd
+import altair as alt
+from vega_datasets import data
+
+# for County choropleths 
+import geopandas as gpd
+from shapely.geometry import Point
+from shapely.geometry import Polygon, MultiPolygon
+
+def general_data_preparation(mass_shootings):
+
+    mass_shootings['Incident Date'] = pd.to_datetime(mass_shootings['Incident Date'])
+    mass_shootings['Month_Year'] = mass_shootings['Incident Date'].dt.to_period('M')
+    mass_shootings = mass_shootings.drop('Incident Date', axis=1)
+ 
+    # grouping BY STATE AND MONTH
+    mass_shootings_states = mass_shootings.groupby(['State', 'Month_Year', 'Region', 'Population']).size().reset_index(name='Total Shootings')
+    
+    # grouping BY REGION AND MONTH
+    mass_shootings_regions = mass_shootings.groupby(['Region', 'Month_Year']).size().reset_index(name='Total Shootings')
+    region_population = mass_shootings_states.drop_duplicates('State').groupby(['Region'])['Population'].sum()
+    mass_shootings_regions = mass_shootings_regions.merge(region_population, on='Region')
+    
+    return mass_shootings_regions, mass_shootings_states
+
+#################### Q1 ####################
+# - linechart, 5 lines, a line per region, being able to choose years
+# - map with regions, select state --> linechart with all states of the region
+
+
+def second_question(mass_shootings_regions):
+    #################### Q2 ####################
     # - grouped barcharts, pair of bars, one for 2014 and the other for the chosen year
     # - scatterplot with 12 points (1 per month), 2014 in x-axis, chosen year in y-axis
 
@@ -51,3 +83,25 @@
     ).add_params(selection_year)
 
     Q2_barchart_final = (Q2_years_barchart + Q2_2014_barchart).facet(column = 'Region:N') | (legend_2014 & legend)
+
+    return Q2_barchart_final 
+
+
+#################### Q3 ####################
+# - map of states, select state and show a map for counties, slider to see years 
+
+def main():
+    mass_shootings = pd.read_csv('MassShootings.csv')
+    counties_gdf = gpd.read_file('Counties.geojson')
+
+    mass_shootings_regions, mass_shootings_states = general_data_preparation(mass_shootings)
+    Q2_barchart_final = second_question(mass_shootings_regions)
+
+    st.set_page_config(layout = 'wide')
+    st.markdown('## Analysis of the evolution of Mass Shootings in the US')
+    st.markdown('**Authors:** Raquel Jolis Carné and Martina Massana Massip')
+
+    st.altair_chart(Q2_barchart_final, use_container_width=True)
+
+if __name__ == '__main__':
+    main()
